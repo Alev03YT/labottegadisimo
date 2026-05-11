78e8221a69c25eeb27db179a30c4046c02f1222f
Alev03YT <55165974+Alev03YT@users.noreply.github.com>
2026-05-11 07:20:28 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 const db = globalThis.__B44_DB__ || { auth:{ isAuthenticated: async()=>false, me: async()=>null }, entities:new Proxy({}, { get:()=>({ filter:async()=>[], get:async()=>null, create:async()=>({}), update:async()=>({}), delete:async()=>({}) }) }), integrations:{ Core:{ UploadFile:async()=>({ file_url:'' }) } } };
 
+import ReviewsSection from "@/components/home/ReviewsSection";
+import FaqSection from "@/components/home/FaqSection";
+import AboutSection from "@/components/home/AboutSection";
 import React, { useEffect, useState } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 
